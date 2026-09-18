91935009a41ac8be763d510c527236bfd0128211
cleitoncoelho <cleiton.alves.560@gmail.com>
2026-09-18 11:46:58 -0300

    #

--- desafio022.py
+++ desafio022.py
@@ -1,2 +1,18 @@
+numeros = []
+soma = 0
+
 for i in range(1, 6):
-    numero = int(input(f'Digite o {i}° número: '))
+    numero = int(input(f'Digite o número {i}: '))
+    soma += numero
+    numeros.append(numero)
+
+media = soma / 5
+
+contador = 0
+
+for n in numeros:
+    if n > media:
+        contador += 1
+
+print(f'A média é {media}')
+print(f'{contador} são maiores que a média')
